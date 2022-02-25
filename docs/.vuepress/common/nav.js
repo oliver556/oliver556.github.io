b0fdaa3a653b016bb8173126c9e32a6306c3ccd1
Jamison <oliver556@163.com>
2022-02-26 00:24:02 +0800

    feat: - 新增《JavaScript 深入系列》笔记迁移 - 新增《Element-UI 实践系列》笔记迁移
    fix:
    - 删除文章

--- docs/.vuepress/common/nav.js
+++ docs/.vuepress/common/nav.js
@@ -1,5 +1,6 @@
-const devNav = require('../nav/devNav');
+const frontEdnNav = require('../nav/frontEndNav');
 const frameNav = require('../nav/frameNav');
+const devNav = require('../nav/devNav');
 const indexNav = require("../nav/indexNav");
 module.exports = [
   {
@@ -8,6 +9,7 @@ module.exports = [
   {
     text: '导航站', link: '/navigation/'
   },
+  frontEdnNav,
   frameNav,
   devNav,
   indexNav, // 索引导航
---
+++ docs/.vuepress/nav/frontEndNav.js
@@ -0,0 +1,20 @@
+// 前端航栏
+module.exports = {
+  text: '前端',
+  link: '/web/', // 目录页链接，此处 link 是 vdoing 主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  items: [
+    // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+    {
+      text: '学习笔记',
+      items: [
+        { text: '《JavaScript 深入系列》笔记', link: '/note/javascript/'}
+      ]
+    },
+    {
+      text: '踩坑笔记',
+      items: [
+        { text: '《Element-UI 实践系列》笔记', link: '/note/element-ui/'}
+      ]
+    }
+  ]
+}
