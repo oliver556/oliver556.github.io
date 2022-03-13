a1b9ddd92ffbf84507a5fbe5a00334c175124805
Jamison <oliver556@163.com>
2022-03-13 15:18:27 +0800

    fix： - 修改 GitHub Action 自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
  msg='来自github actions的自动部署'
  # 替换自己的 GitHub 仓库地址，更改的是 @后面的地址 以及 把Kele-Bingtang 改为自己用户名
- githubUrl=https://oliver556:${GITHUB_TOKEN}@github.com/oliver556.github.io.git
+ githubUrl=https://oliver556:${GITHUB_TOKEN}@github.com/oliver556/oliver556.github.io.git
  git config --global user.name "oliver556"   # 修改为自己的 GitHub 用户名
  git config --global user.email "oliver556@163.com"  # 修改为自己的 GitHub 邮箱，注册时绑定的邮箱
 fi
