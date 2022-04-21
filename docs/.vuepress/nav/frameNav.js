208224f413fdf97639960aef221b56b97c4ebfad
Jamison <oliver556@163.com>
2022-04-21 19:32:40 +0800

    feat： - 新增《Node》笔记 →《NVM 管理多版本 Node》

--- docs/.vuepress/nav/frameNav.js
+++ docs/.vuepress/nav/frameNav.js
@@ -6,7 +6,8 @@ module.exports = {
     {
       text: '进阶服务',
       items: [
-        { text: 'Nginx', link: '/pages/abfa54/'}
+        { text: 'Nginx', link: '/pages/abfa54/'},
+        { text: 'Node', link: '/pages/df2de7/'}
       ]
     }
   ]
