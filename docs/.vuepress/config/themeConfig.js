c2cc417817b551a436a5cc7e5d6510822d0c945c
Jamison <oliver556@163.com>
2022-05-11 16:32:57 +0800

    feat： - 🆕 ：新增《Dev》笔记 →《iTerm2 + Oh My Zsh》

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -11,7 +11,7 @@ module.exports = {
   logo: 'https://cdn.jsdelivr.net/gh/oliver556/image-hosting@master/20220108/avatar_01.3arwwk336g00.jpg', // 导航栏logo
   // repo: 'oliver556/Jamison-blog', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
-  // lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+  lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   // docsDir: 'docs', // 编辑的文件夹
   // editLinks: true, // 启用编辑
   // editLinkText: '编辑此页',
